02535228d70c56c90976a1fc971c6568ee1732cb
Armin <armin.bagheri.1380@gmail.com>
2016-03-29 16:25:27 +0430

    Create TeleSuper.lua

---
+++ plugins/TeleSuper.lua
@@ -0,0 +1,13 @@
+do
+function run(msg, matches)
+  return " TeleSuper V2 Based On Lua \n \n \n Developer: @TelePublic \n Channel: @TeleSuperTeam"
+  end
+return {
+  description = " TeleSuper ", 
+  usage = " TeleSuper ",
+  patterns = {
+    "[#/!][Tt]ele[Ss]uper$",
+  },
+  run = run
+}
+end
