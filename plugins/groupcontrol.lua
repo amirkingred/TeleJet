530f2091f3454a23d66d69d7da5903de682cfd2e
Armin <armin.bagheri.1380@gmail.com>
2016-04-02 13:04:43 +0430

    Update groupcontrol.lua

--- plugins/groupcontrol.lua
+++ plugins/groupcontrol.lua
@@ -1,7 +1,7 @@
 do
 
 -- make sure to set with value that not higher than stats.lua
-local NUM_MSG_MAX = 100
+local NUM_MSG_MAX = 1000
 local TIME_CHECK = 1 -- seconds
 
 local function generate_link(cb_extra, success, result)
